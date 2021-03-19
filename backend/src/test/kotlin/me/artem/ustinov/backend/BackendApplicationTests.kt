fd56f99cfc674c64454f84796531e7f42dfe56f4
Artem Ustinov <ustinov.artem1@huawei.com>
2021-03-19 19:30:50 +0300

    specify test package in backend.yaml

--- backend/src/test/kotlin/me/artem/ustinov/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/me/artem/ustinov/backend/BackendApplicationTests.kt
@@ -5,13 +5,3 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.client.RestTemplateBuilder
 
-@SpringBootTest
-class BackendApplicationTests(@Autowired val restTemplateBuilder: RestTemplateBuilder) {
-
-    @Test
-    fun contextLoads() {
-//        val restTemplate = restTemplateBuilder.build()
-//        val response = restTemplate.getForEntity("/")
-    }
-
-}
